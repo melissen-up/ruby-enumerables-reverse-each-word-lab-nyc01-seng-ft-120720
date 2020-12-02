ea1b68dff797a5c257a4e78b0f708c4794347187
Melissa F <melissa.faulner@gmail.com>
2020-12-02 21:19:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(phrase)
-  phrase.each { |word|
+  split_phrase = phrase.split("")
+  new_phrase = Array.new
+  
+  split_phrase.each do |word|
+  new_phrase << word.reverse
+  end
+  new_phrase.join("")
 end
